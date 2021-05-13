295c712d25cfd9360d6210f20a3fd288d7130a5b
Hitansh Kadakia <hitanshchiragkadakia@gmail.com>
2021-05-13 15:30:58 +0530

    Add files via upload

---
+++ 3Dgraphs.py
@@ -0,0 +1,11 @@
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+x= np.outer(np.linspace(-2,2,10),np.ones(10))
+y=x.copy().T
+z=np.cos(x**2+y**3)
+fig=plt.figure()
+ax=plt.axes(projection='3d')
+ax.plot_surface(x,y,z, cmap='viridis',edgecolor='green')
+ax.set_title('Surface plot happy_coding01')
+plt.show()
